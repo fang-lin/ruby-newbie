b92e273ffbac7cee478b155ba20b9c087aa3582b
linfang <lin.fang@rea-group.com>
2016-08-05 10:02:27 +0800

    [fl & cz] refactor spec.

--- num_to_words/spec/num_to_words_spec.rb
+++ num_to_words/spec/num_to_words_spec.rb
@@ -4,136 +4,58 @@ require_relative '../num_to_words'
 RSpec.describe NumFormatter, type: :model do
 
   describe '#split_to_segments' do
-
-    it 'should return [23, 409, 860, 645, 98]' do
-      result = NumFormatter.to_segments(23409860645098)
-      expect(result).to eq([23, 409, 860, 645, 98])
+    {
+        23409860645098 => [23, 409, 860, 645, 98],
+        0 => [0],
+        1000 => [1, 0],
+        1600000123 => [1, 600, 0, 123],
+    }.each do |key, value|
+      it "should return #{value.to_s}" do
+        result = NumFormatter.to_segments(key)
+        expect(result).to eq(value)
+      end
     end
-
-    it 'should return [0]' do
-      result = NumFormatter.to_segments(0)
-      expect(result).to eq([0])
-    end
-
-    it 'should return [1, 0]' do
-      result = NumFormatter.to_segments(1000)
-      expect(result).to eq([1, 0])
-    end
-
-    it 'should return [1, 600, 0, 123]' do
-      result = NumFormatter.to_segments(1600000123)
-      expect(result).to eq([1, 600, 0, 123])
-    end
-
   end
 
   describe '#to_words' do
     context 'when input < 999' do
-      it 'should return number words' do
-        result = NumFormatter.to_words(999)
-        expect(result).to eq('nine hundred ninety-nine')
+      {
+          0 => 'zero',
+          1 => 'one',
+          2 => 'two',
+          12 => 'twelve',
+          34 => 'thirty-four',
+          50 => 'fifty',
+          99 => 'ninety-nine',
+          100 => 'one hundred',
+          234 => 'two hundred thirty-four',
+          999 => 'nine hundred ninety-nine',
+      }.each do |key, value|
+        it "should return #{value}" do
+          result = NumFormatter.to_words(key)
+          expect(result).to eq(value)
+        end
       end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(0)
-        expect(result).to eq('zero')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(1)
-        expect(result).to eq('one')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(2)
-        expect(result).to eq('two')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(12)
-        expect(result).to eq('twelve')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(34)
-        expect(result).to eq('thirty-four')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(50)
-        expect(result).to eq('fifty')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(99)
-        expect(result).to eq('ninety-nine')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(100)
-        expect(result).to eq('one hundred')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(234)
-        expect(result).to eq('two hundred thirty-four')
-      end
-
-      it 'should return number words' do
-        result = NumFormatter.to_words(999)
-        expect(result).to eq('nine hundred ninety-nine')
-      end
-
     end
   end
 
   describe '#num_to_words' do
-
-    it 'should return zero' do
-      result = NumFormatter.num_to_words(0)
-      expect(result).to eq('zero')
+    {
+        0 => 'zero',
+        5 => 'five',
+        10 => 'ten',
+        11 => 'eleven',
+        50 => 'fifty',
+        100 => 'one hundred',
+        1000 => 'one thousand',
+        12345 => 'twelve thousand, three hundred forty-five',
+        1234567890 => 'one billion, two hundred thirty-four million, five hundred sixty-seven thousand, eight hundred ninety',
+    }.each do |key, value|
+      it "should return #{value}" do
+        result = NumFormatter.num_to_words(key)
+        expect(result).to eq(value)
+      end
     end
-
-    it 'should return five' do
-      result = NumFormatter.num_to_words(5)
-      expect(result).to eq('five')
-    end
-
-    it 'should return ten' do
-      result = NumFormatter.num_to_words(10)
-      expect(result).to eq('ten')
-    end
-
-    it 'should return eleven' do
-      result = NumFormatter.num_to_words(11)
-      expect(result).to eq('eleven')
-    end
-
-    it 'should return fifty' do
-      result = NumFormatter.num_to_words(50)
-      expect(result).to eq('fifty')
-    end
-
-    it 'should return one hundred' do
-      result = NumFormatter.num_to_words(100)
-      expect(result).to eq('one hundred')
-    end
-
-    it 'should return one thousand' do
-      result = NumFormatter.num_to_words(1000)
-      expect(result).to eq('one thousand')
-    end
-
-    it 'should return twelve thousand, three hundred forty-five' do
-      result = NumFormatter.num_to_words(12345)
-      expect(result).to eq('twelve thousand, three hundred forty-five')
-    end
-
-    it 'should return one billion, two hundred thirty-four million, five hundred sixty-seven thousand, eight hundred ninety' do
-      result = NumFormatter.num_to_words(1234567890)
-      expect(result).to eq('one billion, two hundred thirty-four million, five hundred sixty-seven thousand, eight hundred ninety')
-    end
-
   end
 
 end
